064faf85faa0993dff00b0901ea1e3a6ee82b3f8
EmMorrow <emilychandler365@gmail.com>
2017-12-04 19:37:14 -0700

    settings activity

--- app/src/main/java/com/example/emilychandler/family_map/ui/SettingsActivity.java
+++ app/src/main/java/com/example/emilychandler/family_map/ui/SettingsActivity.java
@@ -1,8 +1,33 @@
 package com.example.emilychandler.family_map.ui;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import com.example.emilychandler.family_map.R;
+
 /**
  * Created by emilychandler on 11/22/17.
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Switch lifeStoryLinesSwitch = (Switch) findViewById(R.id.lifeStoryLinesSwitch);
+        Switch spouseLinesSwitch = (Switch) findViewById(R.id.spouseLinesSwitch);
+        Switch familyTreeLinesSwitch = (Switch) findViewById(R.id.familyTreeLinesSwitch);
+
+        Spinner lifeStoryLinesSpinner = (Spinner) findViewById(R.id.lifeStoryLinesSpinner);
+        Spinner spouseLinesSpinner = (Spinner) findViewById(R.id.spouseLinesSpinner);
+        Spinner familyTreeLinesSpinner = (Spinner) findViewById(R.id.familyTreeLinesSpinner);
+
+
+
+    }
+
+
 }
